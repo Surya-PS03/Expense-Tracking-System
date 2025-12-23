7e0284365f9a29f8c7e9a195f2f1d4c6dd8c523d
Surya-PS03 <suryapratapsingh461989@gmail.com>
2025-12-24 00:02:12 +0530

    checked api endpoint works correctly or not

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,7 @@ from database import engine,Base
 import models
 app = FastAPI()
 Base.metadata.create_all(bind = engine)
+
+@app.get("/health")
+async def check_health():
+    return {"health_status":"Good"}
